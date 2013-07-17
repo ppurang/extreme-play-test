4c39152b6ad09b1e74091693baff11ebfc09cea8
Piyush Purang <ppurang@gmail.com>
2013-07-17 09:21:38 +0200

    make tests pending even with given when then and no implementation or skeletal implementation

--- build.sbt
+++ build.sbt
@@ -30,7 +30,8 @@ libraryDependencies ++= {
       "io.spray" % "spray-client" % "1.2-M8",
       "com.typesafe.akka" %% "akka-actor" % "2.2.0-RC1",
       "net.liftweb" %% "lift-json-scalaz7" % liftJsonVersion,
-      "org.scalatest" %% "scalatest" % scalaTestVersion % "test",
+      "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test",
+      //"org.scalatest" %% "scalatest" % scalaTestVersion % "test",
   "org.scalacheck" %% "scalacheck" % "1.10.1" % "test"
      )
 }
--- src/test/scala/PlayerSpec.scala
+++ src/test/scala/PlayerSpec.scala
@@ -1,9 +1,10 @@
 import com.stackmob.newman.response.HttpResponse
 import com.stackmob.newman.SprayHttpClient
+import extreme.play.test.PendingException
 import java.nio.charset.Charset
-import org.scalatest.FeatureSpec
-import org.scalatest.GivenWhenThen
-import org.scalatest.BeforeAndAfter
+import java.util.UUID
+import org.scalatest.exceptions.{TestFailedException, TestPendingException}
+import org.scalatest._
 
 import com.stackmob.newman._
 import com.stackmob.newman.dsl._
@@ -11,36 +12,97 @@ import java.net.URL
 import org.scalatest.matchers.ShouldMatchers
 import scalaz.CharSet
 
+
 class PlayerSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfter with ShouldMatchers {
   implicit val client = new SprayHttpClient()
-  //implicit val client = new ApacheHttpClient()
 
   before {
 
   }
+  def ??? = throw new PendingException("pending exception", null, false, false)
+  val uuid = UUID.randomUUID()
 
-  feature("Player registration and withdrawl") {
-    info("It is all about player's lifecycle")
-    //info("I should be able to register myself")
+  feature("Player registration") {
+    info("allow players to register at http://localhost:9000/player/{uuid}")
 
-    scenario("register") {
-
-      Given("A valid payload")
+    scenario("valid register") {
+      Given("a valid payload")
       val payload =
         """{
           | "name" : "whatever",
           | "uri" : "http://someotherhost:8080/play"
           |}
         """.stripMargin
-      When("the payload is posted to the correct api endpoint")
-      val url = new URL("http://localhost:9000/player")
-      val post = POST(url)
-      post.addBody(payload)
+      When("it is registered with a valid uuid")
+      val url = new URL(s"http://localhost:9000/player/$uuid")
+      val put = PUT(url).addBody(payload).prepareAsync
 
       Then("a valid location is returned")
-      post.prepareAsync.map(_.map(_.headers.map(_.list.filter(_._1 == "Location"))).get.headOption).unsafePerformIO() should be (Option(List("Location" -> "http://localhost:9000/player/1" )))
+      val location = put.map(_.map(_.headers.map(_.list.filter(_._1 == "Location"))).get.headOption)
+      location.unsafePerformIO() should be (Option(List("Location" -> s"http://localhost:9000/player/$uuid" )))
+      And("the player can be retrieved")
+      GET(url).executeUnsafe.code should be(200)
     }
 
+    scenario("invalid uuid")(pendingUntilFixed{
+      Given("an invalid uuid")
+      When("it is registered")
+      Then("bad request is raised")
+      ???
+    })
+
+    scenario("invalid payload")(pendingUntilFixed {
+      Given("an invalid payload")
+      When("when it is registered with a valid uuid")
+      Then("bad request is raised")
+      ???
+    })
+
   }
+
+  //should this even be allowed?
+  feature("Player withdrawl") {
+    info("allow players to withdraw  at http://localhost:9000/player/{uuid}")
+    //info("I should be able to register myself")
+
+    scenario("withdrawl") {
+      Given("a valid player uuid")
+      val wuuid = UUID.randomUUID()
+      val payload =
+        """{
+          | "name" : "whatever",
+          | "uri" : "http://someotherhost:8080/play"
+          |}
+        """.stripMargin
+      val url = new URL(s"http://localhost:9000/player/$wuuid")
+      PUT(url).addBody(payload).executeUnsafe.code should be(201)
+
+      When("the player withdraws using delete")
+      DELETE(url).executeUnsafe
+      Then("that player isn't registered anymore")
+      GET(url).executeUnsafe.code should be(404)
+    }
+
+    scenario("invalid uuid")(pendingUntilFixed{
+      Given("an invalid uuid")
+      When("it is withdrawn")
+      Then("bad request is raised")
+      ???
+    })
+
+    scenario("not found")(pendingUntilFixed {
+      Given("a valid uuid that hasn't been registered")
+      When("it is withdrawn")
+      Then("not found is raised")
+      ???
+    })
+
+  }
+
+
+
+
+
+
 }
 
